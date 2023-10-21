824472be87be6dbfe96b2e3cdf972ea4a91008cc
psterner <psterner@hotmail.com>
2023-10-21 22:15:20 +0200

    Add components Home and Navbar. Add styling

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+    </div>
+  );
+};
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <nav className="navBar">
+      <h1>Brizzy Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
