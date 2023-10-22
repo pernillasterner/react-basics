3a9b48071b6635d3459f0be62937f1431a54760d
psterner <psterner@hotmail.com>
2023-10-22 17:08:36 +0200

    Pass data from parent to child component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BlogList } from "./BlogList";
 
 export const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -12,20 +13,9 @@ export const Home = () => {
     },
   ]);
 
-  //   const outputData = () => {
-  //     return blogs.forEach((blog) => {
-  //       <h2>{blog.title}</h2>;
-  //       <p>{blog.body}</p>;
-  //     });
-  //   };
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
