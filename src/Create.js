119090cb3b5f2d01955b28d1c60f8a7d4202f1e5
psterner <psterner@hotmail.com>
2023-10-25 12:40:18 +0200

    Sending a Post Request and Update the data.json

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,30 @@ export const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    // doens't refresh the page
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    // make post request. asynchronously
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added successfully");
+      setIsPending(false);
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -28,10 +47,8 @@ export const Create = () => {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
