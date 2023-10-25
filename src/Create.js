8f3f7a33b81779662765578ee91181f8c7fb2955
psterner <psterner@hotmail.com>
2023-10-25 11:29:03 +0200

    Create form and use State to keep track of input values

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 export const Create = () => {
+  // state to keep track the title
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
-
-export default Create;
