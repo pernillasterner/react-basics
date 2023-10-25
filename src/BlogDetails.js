7375df81a496e739363184cf1b0f1a39e13183f3
psterner <psterner@hotmail.com>
2023-10-25 10:22:42 +0200

    Make dynamic bloglist url:s

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,27 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 export const BlogDetails = () => {
   const { id } = useParams();
+  // Getting data, error msg and pending from useFetch component
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {/* Is pending true log loading */}
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
