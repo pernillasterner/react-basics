e4af6faa77b8291eefd990323ff18bdcefb1a792
psterner <psterner@hotmail.com>
2023-10-25 09:41:05 +0200

    Abort updating component

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+// Run server
+// npx json-server --watch data/db.json --port 8000
 export const useFetch = (url) => {
   // State properties
   const [data, setData] = useState(null);
@@ -7,10 +9,12 @@ export const useFetch = (url) => {
   const [error, setError] = useState(null);
   // Fetching blog list data from the server
   useEffect(() => {
+    // Associating this controller with the fetch request
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
-          console.log(res);
           if (!res.ok) {
             throw Error("couldn't fetch blog list");
           }
@@ -24,10 +28,16 @@ export const useFetch = (url) => {
         })
         // Catches network errors and fires a function
         .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url]);
   return { data, isPending, error };
 };
